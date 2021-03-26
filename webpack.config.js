7fbbe3893aa11b13105d7b80a077e77c673a256a
maxperei <maxime.pereira@synolia.com>
2021-03-26 16:25:48 +0100

    Fix assets directory structure to generate `public/_themes` during `sylius:theme:assets:install` See https://github.com/Sylius/SyliusThemeBundle/blob/master/docs/important_changes.md#assets Related to Sylius/SyliusThemeBundle#91

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const Encore = require('@symfony/webpack-encore');
 Encore
   .setOutputPath('public/bootstrap-theme')
   .setPublicPath('/bootstrap-theme')
-  .addEntry('app', './themes/BootstrapTheme/assets/app.js')
+  .addEntry('app', './themes/BootstrapTheme/public/app.js')
   .disableSingleRuntimeChunk()
   .cleanupOutputBeforeBuild()
   .enableSassLoader()
